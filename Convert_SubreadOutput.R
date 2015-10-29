cb7896363d07f49365ef29b83b671e109df7f4a8
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2015-10-29 13:11:06 +0100

    much faster now

--- Convert_SubreadOutput.R
+++ Convert_SubreadOutput.R
@@ -7,11 +7,13 @@ require(dplyr)
 require(argparser)
 })
 
+
 ## Add arguments
 p <- arg_parser("Read FeatureCount output and convert to DEXSeq acceptable format")
 p <- add_argument(p,"--fcout",help = "Featurecounts output file (using DEXSeq-flattened GTF file)")
 p <- add_argument(p,"--names",help = "Sample names (In same order as column names in output file)")
 p <- add_argument(p,"--outfile",help = "HTSeq-like output file to write back.")
+p <- add_argument(p,"--threads",help = "Number of cores to use (leave empty for one)")
 
 ## Parse the arguments
 argv <- parse_args(p)
@@ -19,6 +21,8 @@ fcout <- argv$fcout
 samplenames <- argv$names # samplenames seperated by comma ,
 outfile <- argv$outfile
 out <- argv$out
+threads <-argv$threads
+
 print("branch faster")
 
 system.time({
@@ -32,7 +36,13 @@ system.time({
           filter(df,V1 == name) %>% mutate(V1 = paste0(V1,":",sprintf("%03.0f",1:nrow(.)))) %>% 
                   return()
   }
-  plyr::ldply(genes, getdata,df = df) -> outdf
+  if(is.null(threads)){
+          plyr::ldply(genes, getdata,df = df) -> outdf
+  } else {
+          cl <- parallel::makeCluster(threads,type = "FORK")
+          parallel::mclapply(genes, getdata,df = df) %>% plyr::ldply(data.frame) -> outdf
+  }
+  
 
   # write back the output
   sink(outfile)
